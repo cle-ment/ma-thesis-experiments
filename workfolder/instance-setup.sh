b98a2ef3ddcd86027f9d43fddd967ff22cac6e0e
cle-ment <cle-ment@gmx.de>
2016-08-09 00:13:11 +0300

    hopefully fixed loading env for running correct python version

--- workfolder/instance-setup.sh
+++ workfolder/instance-setup.sh
@@ -3,17 +3,23 @@
 # clone repo (replace password)
 git clone https://cle-ment:XXXXXXX@github.com/cle-ment/thesis-experiments /home/ubuntu/thesis-experiments
 
+# change permissions
+chmod -R 777 /home/ubuntu/thesis-experiments/
+chown -R ubuntu:ubuntu /home/ubuntu/thesis-experiments/
+
 # move data into workfolder
-mv /home/ubuntu/2016-07-aws-experiments/workfolder/data /home/ubuntu/thesis-experiments/workfolder/data
+mv /home/ubuntu/thesis-data /home/ubuntu/thesis-experiments/workfolder/data
 
-# remove old experiments folder
-rm -rf /home/ubuntu/2016-07-aws-experiments
-
-# make output dir
-mkdir thesis-experiments/output
+# make output dir and change its rights
+mkdir /home/ubuntu/thesis-experiments/output
+chmod -R 777 /home/ubuntu/thesis-experiments/output
+chown -R ubuntu:ubuntu /home/ubuntu/thesis-experiments/output
 
 # copy previous output to continue computation (if available)
 scp -r clemens@cwestrup.de:/home/clemens/thesis/output/ /home/ubuntu/thesis-experiments/
 
+# prepare .bash_profile to load enviroment when execiting command as user 'ubuntu'
+mv /home/ubuntu/thesis-experiments/--.bash_profile /home/ubuntu/thesis-experiments/.bash_profile
+
 # run computation
-python /home/ubuntu/thesis-experiments/workfolder/eval_doc2vec.py
+sudo su --login ubuntu -c "cd /home/ubuntu/thesis-experiments/workfolder; python eval_doc2vec.py"
